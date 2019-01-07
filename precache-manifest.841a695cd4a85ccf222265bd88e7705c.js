ca5068a844ee2e2844900ab14df1ce76494b605d
richardrun <richardrunlpl@gmail.com>
2019-01-06 19:28:00 -0800

    Updates

---
+++ precache-manifest.841a695cd4a85ccf222265bd88e7705c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/profile-web/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0a6b47a8f783c7feb9c2",
+    "url": "/profile-web/static/js/runtime~main.0a6b47a8.js"
+  },
+  {
+    "revision": "b281c1ddc41e99e5bc6c",
+    "url": "/profile-web/static/js/main.b281c1dd.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/profile-web/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "b281c1ddc41e99e5bc6c",
+    "url": "/profile-web/static/css/main.437d5252.chunk.css"
+  },
+  {
+    "revision": "7ad285e968c2a2ea71c7cbedb64249ca",
+    "url": "/profile-web/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/profile-web/precache-manifest.89fe1f2a027c10076f96b00e4a90881f.js"
+  "/profile-web/precache-manifest.841a695cd4a85ccf222265bd88e7705c.js"
 );
 
 workbox.clientsClaim();
